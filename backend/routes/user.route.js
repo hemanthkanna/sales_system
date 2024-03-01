6cfd3cc0090977ca81dff96ceae10595faf90700
hemanthkanna <hemanthkanna20@gmail.com>
2024-03-01 14:29:59 +0530

    User Authorization

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -16,17 +16,15 @@ const { isAdmin } = require("../utils/authroizedRole");
 const router = express.Router();
 
 router.route("/").post(createUser);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
 router.route("/").get(getUsers);
-router.route('/login').post(login);
-router.route('/logout').get(logout);
-router.route("/findOne/").get(findOneUser);
-router.route("/findOrCreate/").get(findOrCreateUser);
-router.route("/findAndCount").get(findAndCountUser);
-router.route("/:id").get(getSingleUser);
-router.route("/:id").put(isAdmin,updateUser);
-router.route("/:id").delete(isAdmin,deleteUser);
+router.route("/findOne/").get(isAdmin, findOneUser);
+router.route("/findOrCreate/").get(isAdmin, findOrCreateUser);
+router.route("/findAndCount").get(isAdmin, findAndCountUser);
+router.route("/:id").get(isAdmin, getSingleUser);
+router.route("/:id").put(isAdmin, updateUser);
+router.route("/:id").delete(isAdmin, deleteUser);
 router.route("/restore/:id").post(restoreUser);
-
-
 
 module.exports = router;
--- backend/utils/authroizedRole.js
+++ backend/utils/authroizedRole.js
@@ -3,6 +3,10 @@ exports.isAdmin = (req, res, next) => {
     return next();
   }
 
+  if(!req.user) {
+    return res.status(404).json({ message: 'Login first to access this resources' });
+  }
+
   res.status(200).json({
     message: "Access Forbidden only Admin can access this Resources",
   });
